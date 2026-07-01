4abea4c577005a652d1db396a843fa2f365c5fc1
aysuulussoy <aysu3561@gmail.com>
2026-07-01 15:41:43 +0300

    chore: disable default security user auto configuration
    Disable Spring Boot default generated security user because the project uses JWT authentication.

--- src/main/java/com/yurticicargo/personnel_task_management/PersonnelTaskManagementApplication.java
+++ src/main/java/com/yurticicargo/personnel_task_management/PersonnelTaskManagementApplication.java
@@ -2,12 +2,12 @@ package com.yurticicargo.personnel_task_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class PersonnelTaskManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonnelTaskManagementApplication.class, args);
 	}
-
 }
